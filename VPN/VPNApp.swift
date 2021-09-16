30bbf9af6f7ad5758fbc5883f1cf0dd7342541ab
Jigar Dave <jdavenz8@gmail.com>
2021-09-16 09:28:44 +0530

    Initial Commit

---
+++ VPN/VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNApp.swift
+//  VPN
+//
+//  Created by jigar dave on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
